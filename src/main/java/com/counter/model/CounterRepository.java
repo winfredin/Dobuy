1620961d9fee5ee209e92396072d81b326130952
John YU <rin020255@gmail.com>
2024-11-25 10:56:32 +0800

    新增COUNTERVO

---
+++ src/main/java/com/counter/model/CounterRepository.java
@@ -0,0 +1,28 @@
+package com.counter.model;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Repository
+public interface CounterRepository extends JpaRepository<CounterVO, Integer> {
+    // 這裡可以根據需求定義額外的方法，例如根據櫃位帳號查找
+    CounterVO findByCounterAccount(String counterAccount);
+    
+    // 檢查帳號是否重複
+    boolean existsByCounterAccount(String counterAccount);
+    
+    // 檢查統一編號是否重複
+    boolean existsByCounterUbn(String counterUbn);
+
+    // 查找所有特定狀態的櫃位
+    List<CounterVO> findByCounterStatus(Integer counterStatus);
+    
+    CounterVO findByCounterUbn(String counterUbn);
+    
+
+}
---
+++ src/main/java/com/counter/model/CounterService.java
@@ -0,0 +1,56 @@
+package com.counter.model;
+
+import com.counter.model.CounterVO;
+import com.counter.model.CounterRepository;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@Service("counterService")
+public class CounterService {
+
+    @Autowired
+    private CounterRepository repository;
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    public void addCounter(CounterVO counterVO) {
+        if (repository.existsByCounterAccount(counterVO.getCounterAccount())) {
+            throw new IllegalArgumentException("櫃位帳號已存在，請使用其他帳號。");
+        }
+        repository.save(counterVO);
+    }
+
+    public void updateCounter(CounterVO counterVO) {
+        repository.save(counterVO);
+    }
+
+
+
+    public CounterVO getOneCounter(Integer counterNo) {
+        Optional<CounterVO> optional = repository.findById(counterNo);
+        return optional.orElse(null);
+    }
+    
+    public List<CounterVO> getAll(){
+    	return repository.findAll();
+    }
+
+    public List<CounterVO> getAll(Map<String, String[]> map) {
+        return repository.findAll();
+    }
+    
+    public boolean isCounterUbnExists(String counterUbn) {
+        return repository.existsByCounterUbn(counterUbn);
+    }
+
+    public boolean isCounterAccountExists(String counterAccount) {
+        return repository.existsByCounterAccount(counterAccount);
+    }
+}
