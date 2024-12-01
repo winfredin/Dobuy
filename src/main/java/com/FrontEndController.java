caefd0a1324ddf308261c6fdecff47876f7c7d1a
Piko <piko2067@gmail.com>
2024-12-01 16:37:00 +0800

    piko衝突處理

--- src/main/java/com/FrontEndController.java
+++ src/main/java/com/FrontEndController.java
@@ -1,10 +1,24 @@
 package com;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.goods.model.GoodsService;
+import com.goods.model.GoodsVO;
+import com.goodstype.model.GoodsTypeService;
+import com.goodstype.model.GoodsTypeVO;
 
 @Controller
 public class FrontEndController {
+	
+	@Autowired
+	GoodsService goodsSvc;
+	@Autowired
+	GoodsTypeService goodstSvc;
 	 @GetMapping("member")
 	    public String getMemberPage() {
 	        return "front-end/normalpage/member"; // 對應 templates/content/profile.html
@@ -14,7 +28,11 @@ public class FrontEndController {
 	        return "front-end/normalpage/homepage"; // 對應 templates/content/profile.html
 	    }
 	 @GetMapping("goodspage")
-	    public String getgoodspagePage() {
+	    public String getgoodspagePage(Model model) {
+		 List<GoodsVO> list = goodsSvc.getAll();
+		 List<GoodsTypeVO> glist = goodstSvc.getAll();
+		 model.addAttribute("list",list);
+		 model.addAttribute("glist",glist);
 	        return "front-end/normalpage/goodspage"; // 對應 templates/content/profile.html
 	    }
 	    
