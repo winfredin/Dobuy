0981bbcbd34b86bb4f2f18ebdd08028c70bdb462
CBU <he01314906@gmail.com>
2024-12-05 02:39:54 +0800

    後台抓取會員 並修改狀態

--- src/main/java/com/member/model/MemberRepository.java
+++ src/main/java/com/member/model/MemberRepository.java
@@ -2,7 +2,12 @@ package com.member.model;
 
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -14,5 +19,11 @@ public interface MemberRepository extends JpaRepository<MemberVO, Integer>{
 //	winfred===================================================================================以下
 	Optional<MemberVO> findByMemAccount(String memAccount);
 //	winfred===================================================================================以上
+
+	
+	@Modifying
+	@Transactional
+	@Query("UPDATE MemberVO m SET m.memStatus = :memStatus WHERE m.memNo = :memNo")
+	void updateStatus(@Param("memStatus") int memStatus, @Param("memNo") int memNo);
 
 }
--- src/main/java/com/member/model/MemberService.java
+++ src/main/java/com/member/model/MemberService.java
@@ -1,6 +1,11 @@
 package com.member.model;
 
+import java.util.List;
 import java.util.Optional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,6 +15,7 @@ public class MemberService {
 
 	@Autowired
 	MemberRepository memberRepository;
+
 
 	public void addMem(MemberVO memberVO) {
 		if (memberVO.getMemStatus() == null) {
@@ -25,7 +31,10 @@ public class MemberService {
 	public boolean validateLogin(String memAccount, String memPassword) {
 		return memberRepository.findByMemAccountAndMemPassword (memAccount, memPassword).isPresent();
 	}
-	
+	public List<MemberVO> getAll() {
+		List<MemberVO> memberList= memberRepository.findAll();
+		return memberList;
+	}
 	
     // winfred
     public Optional<MemberVO> findById(Integer memNo) { //1130
@@ -36,5 +45,10 @@ public class MemberService {
         Optional<MemberVO> optionalMember = memberRepository.findByMemAccount(memAccount);
         return optionalMember.orElse(null);
     }
-
+  
+  
+    public void upStatus(Integer memNo,Integer memStatus) { 	
+    	   memberRepository.updateStatus(memStatus,memNo);
+    	   
+    }
 }
