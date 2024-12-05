50d7d9880bbfc2e97ff49cd444681c65b678051c
Gary <fred52870@gmail.com>
2024-12-05 17:34:28 +0800

    push

--- src/main/java/com/usedecpay/model/EcpayService.java
+++ src/main/java/com/usedecpay/model/EcpayService.java
@@ -2,13 +2,20 @@ package com.usedecpay.model;
 
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.usedorder.model.UsedOrderService;
 
 import ecpay.payment.integration.AllInOne;
 import ecpay.payment.integration.domain.AioCheckOutOneTime;
 
 @Service
 public class EcpayService {
+	
+	@Autowired
+	UsedOrderService usedOrderService;
+	
 	
 	public static String genAioCheckOutOneTime(){
 		
@@ -18,13 +25,15 @@ public class EcpayService {
 		
 		AioCheckOutOneTime obj = new AioCheckOutOneTime();
 		
+		//獲取當前交易時間 "2017/01/01 08:05:23"
+		
 		obj.setMerchantTradeNo(uuId);
 		obj.setMerchantTradeDate("2017/01/01 08:05:23");
 		obj.setTotalAmount("50");
 		obj.setTradeDesc("test Description");
 		obj.setItemName("TestItem");
 		obj.setReturnURL("http://localhost:8080/used/select_page");
-		obj.setOrderResultURL(uuId);
+		obj.setOrderResultURL("http://localhost:8080/used/select_page");
 		obj.setNeedExtraPaidInfo("N");
 		obj.setRedeem("N");
 		obj.setClientBackURL("http://localhost:8080/used/select_page");
