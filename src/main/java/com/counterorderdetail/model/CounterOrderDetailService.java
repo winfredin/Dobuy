c6b0e6616897791ef3e91426c2fb6e0873aa55b2
Winfred <e11010129@gmail.com>
2024-12-06 22:39:54 +0800

    增加CounterOrderDetail

---
+++ src/main/java/com/counterorderdetail/model/CounterOrderDetailService.java
@@ -0,0 +1,42 @@
+package com.counterorderdetail.model;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("counterOrderDetailService")
+public class CounterOrderDetailService {
+	         
+   @Autowired
+   CounterOrderDetailRepository repository;
+   
+   @Autowired
+   private SessionFactory sessionFactory;
+
+   public void addCounterOrderDetail(CounterOrderDetailVO counterOrderDetailVO) {
+       repository.save(counterOrderDetailVO);
+   }
+
+   public void updateCounterOrderDetail(CounterOrderDetailVO counterOrderDetailVO) {
+       repository.save(counterOrderDetailVO);
+   }
+
+   public void deleteCounterOrderDetail(Integer counterOrderDetailNo) {
+       if (repository.existsById(counterOrderDetailNo))
+           repository.deleteByCounterOrderDetailNo(counterOrderDetailNo);
+   }
+
+   public CounterOrderDetailVO getOneCounterOrderDetail(Integer counterOrderDetailNo) {
+       Optional<CounterOrderDetailVO> optional = repository.findById(counterOrderDetailNo);
+       return optional.orElse(null);
+   }
+
+   public List<CounterOrderDetailVO> getAll() {
+       return repository.findAll();
+   }
+}
