aa4d2a74de3eb01b502b5552d0d0ca10e972177e
John YU <rin020255@gmail.com>
2024-12-06 12:06:21 +0800

    櫃位常見問題頁面

--- src/main/java/com/faq/controller/FaqController.java
+++ src/main/java/com/faq/controller/FaqController.java
@@ -1,16 +1,24 @@
 package com.faq.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.counter.model.CounterService;
 import com.counter.model.CounterVO;
+import com.coupon.model.CouponVO;
 import com.faq.model.FaqService;
+import com.faq.model.FaqVO;
 
 
 
@@ -20,26 +28,49 @@ public class FaqController {
 	
 	
     
-//	@Autowired
-//	FaqService faqsvc;
-//	
-//	
-//	@Autowired
-//	CounterService counterSvc;
-//	
-//	TEST
-//    @GetMapping("/listCounterFaq")
-//    public String listAllCoupon(HttpSession session, Model model) {
-//    	//櫃位優惠券登錄確認
-//        CounterVO counter = (CounterVO) session.getAttribute("counter");
-//        if (counter == null) {
-//            return "redirect:/counter/login";
-//        } else {
-//            // 其他邏輯
-//            model.addAttribute("counter", counterSvc.getOneCounter(counter.getCounterNo()));
-//            model.addAttribute("counterCouponListData", faqsvc.getOneCounterFaq(counter.getCounterNo()));
-//            return "vendor-end/coupon/listCounterFaq";
-//        }
-//    }
+	@Autowired
+	FaqService faqsvc;
+	
+	
+	@Autowired
+	CounterService counterSvc;
+	
+//	櫃位FAQ管理
+    @GetMapping("/listCounterFaq")
+    public String listAllCoupon(HttpSession session, Model model) {
+    	//櫃位優惠券登錄確認
+        CounterVO counter = (CounterVO) session.getAttribute("counter");
+        if (counter == null) {
+            return "redirect:/counter/login";
+        } else {
+            // 其他邏輯
+            model.addAttribute("counter", counterSvc.getOneCounter(counter.getCounterNo()));
+            model.addAttribute("counterFaqListData", faqsvc.getOneCounterFaq(counter.getCounterNo()));
+            return "vendor-end/faq/listCounterFaq";
+        }
+    }
+    
+    @ModelAttribute("counterFaqListData")
+    protected List<FaqVO> CounterReferenceListData(HttpSession session, Model model) {
+        CounterVO counter = (CounterVO) session.getAttribute("counter");
+        if (counter != null) {
+        	List<FaqVO> list = faqsvc.getOneCounterFaq(counter.getCounterNo());
+            return list;
+        } else {
+            // 如果counter為null，返回一個空列表或處理錯誤
+            model.addAttribute("error", "未登錄或Session信息遺失");
+            return new ArrayList<>(); // 或者其他適當的錯誤處理
+        }
+    }
+    
+    @PostMapping("listCounterFaq_ByCompositeQuery")
+    public String listCounterFaq(HttpSession session ,HttpServletRequest req, Model model) {
+        CounterVO counter = (CounterVO) session.getAttribute("counter");
+        List<FaqVO> list = faqsvc.getOneCounterFaq(counter.getCounterNo());
+        model.addAttribute("counterFaqListData", list); // for listAllEmp.html 第85行用
+        return "vendor-end/faq/listCounterFaq";
+    }
+    
+//	櫃位FAQ管理
 
 }
